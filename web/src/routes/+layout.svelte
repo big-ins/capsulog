d0bc5f4c2fd6ba658061276f2d786f8829d8c5b2
ouchi <ouchi421taiga@gmail.com>
2026-09-23 17:35:20 +0000

    feat: PWA のアイコンと manifest を置く
    - アイコンは暦にカプセルを乗せた形。Android 用に maskable 版を別に持つ
    - SvelteKit のテンプレートのままだった favicon を差し替える

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import AppHeader from '$lib/common/components/AppHeader.svelte';
 	import BottomNav from '$lib/common/components/BottomNav.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <AppHeader />
 <!-- ページ末尾の余白はここで持つ。狭い画面では下部ナビの高さも含める -->
 <div class="pb-28 lg:pb-16">
